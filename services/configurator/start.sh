ddf0e744315d71c45923390aad0b3e8d313ac876
Ellis Barnwell <ellis@carbon.coop>
2023-12-05 10:04:02 +0000

    change wait time before doing iptables rule and put mosquitto on docker network

--- services/configurator/start.sh
+++ services/configurator/start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-sleep 30
+sleep 60
 
+# Setup IP tables rule to allow 2 way comms between docker network and wireless-repeater block
 iptables -I nm-sh-fw-wlan0 -o wlan0 -s 172.18.4.0/24 -d 10.42.0.0/24 -j ACCEPT
 
 # Keep this at the end
