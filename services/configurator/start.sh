e0648227649ca732aa52b085cdc203db93544888
Ellis Barnwell <ellis@carbon.coop>
2024-01-22 16:51:12 +0000

    Updates to iptables configurator

--- services/configurator/start.sh
+++ services/configurator/start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-sleep 60
+sleep 180
 
 # Setup IP tables rule to allow 2 way comms between docker network and wireless-repeater block
-iptables -I nm-sh-fw-wlan0 -o wlan0 -s 172.18.4.0/24 -d 10.42.0.0/24 -j ACCEPT
+iptables-legacy -I nm-sh-fw-wlan0 -o wlan0 -s 172.18.4.0/24 -d 10.42.0.0/24 -j ACCEPT
 
 # Keep this at the end
 tail -f
