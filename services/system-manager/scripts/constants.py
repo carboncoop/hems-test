3e645af28c4f9e501d7d2058a4bee8fc3e03aed9
Ellis Barnwell <ellis@carbon.coop>
2024-02-28 14:10:34 +0000

    Fix typo in application url

--- services/system-manager/scripts/constants.py
+++ services/system-manager/scripts/constants.py
@@ -6,7 +6,7 @@ APP_ID = os.getenv("BALENA_APP_ID")
 APP_NAME = os.getenv("BALENA_APP_NAME")
 
 STATUS_URL = SUPERVISOR_ADDRESS + "/v2/state/status?apikey=" + API_KEY
-APPICATION_URL = SUPERVISOR_ADDRESS + "/v2/applications/state?apikey=" + API_KEY
+APPLICATION_URL = SUPERVISOR_ADDRESS + "/v2/applications/state?apikey=" + API_KEY
 STOP_URL = SUPERVISOR_ADDRESS + "/v2/applications/" + APP_ID + "/stop-service?apikey=" + API_KEY
 RESTART_URL = SUPERVISOR_ADDRESS + "/v2/applications/" + APP_ID + "/restart-service?apikey=" + API_KEY
 REBOOT_URL = SUPERVISOR_ADDRESS + "/v1/reboot?apikey=" + API_KEY
