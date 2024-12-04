14e139393d683c896e4a8ffca9b257e0ba4f712b
Ellis Barnwell <ellis@carbon.coop>
2024-12-04 15:01:59 +0000

    only make backups on first day of month and keep for 3 months

--- services/homeassistant/config/scripts/purge_backups.sh
+++ services/homeassistant/config/scripts/purge_backups.sh
@@ -3,8 +3,5 @@
 # Folder to clean up
 TARGET_FOLDER="/config/backups"
 
-# Age threshold in minutes
-AGE_DAYS=2  # Change this to the desired age threshold
-
 # Find and delete files older than the specified age
-find "$TARGET_FOLDER" -type f -mtime +$AGE_DAYS -exec rm -f {} \;
+find "$TARGET_FOLDER" -type f -mtime +90 -exec rm -f {} \;
